35cba5d80eed35792fe16bcb5e657d966fc6963d
AndrewPochapsky <andrew.pochapsky@gmail.com>
2022-11-21 18:25:42 -0600

    Small changes

--- weeks/week1/src/lib.rs
+++ weeks/week1/src/lib.rs
@@ -13,9 +13,9 @@ fn _decrypt(_key: Vec<u8>, _tweak: Vec<u8>, _ciphertext: Vec<u8>) -> Vec<u8> {
     return vec![];
 }
 
-/// Encrypts chosen `plaintext` with the chosen `key` using the Vigenere cipher. It is assumed that
+/// Encrypts chosen `plaintext` with the chosen `key` using the Vignere cipher. It is assumed that
 /// both of these inputs only contain letters from the alphabet.
-pub fn vigenere_encrypt(key: String, plaintext: String) -> String {
+pub fn vignere_encrypt(key: &str, plaintext: &str) -> String {
     let key = key.to_lowercase();
     let plaintext = plaintext.to_lowercase();
 
@@ -33,9 +33,9 @@ pub fn vigenere_encrypt(key: String, plaintext: String) -> String {
     return cipher_vec.into_iter().collect();
 }
 
-/// Decrypts chosen `ciphertext` with the chosen `key` using the Vigenere cipher. It is assumed that
+/// Decrypts chosen `ciphertext` with the chosen `key` using the Vignere cipher. It is assumed that
 /// both of these inputs only contain letters from the alphabet.
-pub fn vigenere_decrypt(key: String, ciphertext: String) -> String {
+pub fn vignere_decrypt(key: &str, ciphertext: &str) -> String {
     let key = key.to_lowercase();
     let ciphertext = ciphertext.to_lowercase();
 
--- weeks/week1/src/testing/tests.rs
+++ weeks/week1/src/testing/tests.rs
@@ -1,14 +1,12 @@
-use crate::{vigenere_decrypt, vigenere_encrypt};
+use crate::{vignere_decrypt, vignere_encrypt};
 
 #[test]
-fn test_vigenere() {
+fn test_vignere() {
     let plaintext = "attackatdawn".to_string();
     let key = "LEMON".to_string();
 
-    let ciphertext = vigenere_encrypt(key.clone(), plaintext);
+    let ciphertext = vignere_encrypt(&key, &plaintext);
     assert_eq!("lxfopvefrnhr", ciphertext);
 
-    // k_i = M = 11
-    // c_i = f = 5
-    assert_eq!("attackatdawn", vigenere_decrypt(key, ciphertext))
+    assert_eq!("attackatdawn", vignere_decrypt(&key, &ciphertext))
 }
